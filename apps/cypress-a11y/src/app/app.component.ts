4736aef69426ced96dbbe5522a109cdeb5b5d9d1
Rachel Noccioli <rachel.noccioli@oasisdigital.com>
2020-10-20 16:30:07 -0400

    initial commit

---
+++ apps/cypress-a11y/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-a11y-testing-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cypress-a11y';
+}
