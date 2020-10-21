c69465d82c0bc1dc9609bb598559433b350fd7ab
Rachel Noccioli <rachel.noccioli@oasisdigital.com>
2020-10-20 23:54:33 -0400

    Install cypress-axe with basic test for page load

--- apps/cypress-a11y-e2e/src/integration/app.spec.ts
+++ apps/cypress-a11y-e2e/src/integration/app.spec.ts
@@ -6,4 +6,15 @@ describe('Astronaut Directory', () => {
   it('should display a header', () => {
     header().should('contain', 'Astronaut Directory');
   });
+
+  describe('a11y', () => {
+    beforeEach(() => {
+      cy.injectAxe();
+    });
+    it('should not have detectable a11y errors on load', () => {
+      cy.checkA11y(null, {
+        includedImpacts: ['critical'],
+      });
+    });
+  });
 });
