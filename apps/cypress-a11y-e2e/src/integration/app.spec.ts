04b6bd859edddab175765e0d0d205f966ae7a928
Rachel Noccioli <rachel.noccioli@oasisdigital.com>
2020-10-20 23:54:52 -0400

    Add tests for add astronaut form

--- apps/cypress-a11y-e2e/src/integration/app.spec.ts
+++ apps/cypress-a11y-e2e/src/integration/app.spec.ts
@@ -1,4 +1,8 @@
-import { header } from '../support/app.po';
+import {
+  addAstronautButton,
+  addAstronautForm,
+  header,
+} from '../support/app.po';
 
 describe('Astronaut Directory', () => {
   beforeEach(() => cy.visit('/'));
@@ -14,6 +18,31 @@ describe('Astronaut Directory', () => {
     it('should not have detectable a11y errors on load', () => {
       cy.checkA11y(null, {
         includedImpacts: ['critical'],
+      });
+    });
+
+    // check full page after opening modal
+    it('should not have detectable a11y errors after opening the add astronaut modal', () => {
+      addAstronautButton().click();
+      addAstronautForm().should('exist');
+      cy.checkA11y(
+        {},
+        {
+          rules: {
+            // This is not handled correctly by cdkFocusTrap
+            // https://github.com/angular/components/issues/9035
+            'aria-hidden-focus': { enabled: false },
+          },
+        }
+      );
+    });
+
+    // scope tests to modal
+    it('should not have detectable a11y errors on the add astronaut modal', () => {
+      addAstronautButton().click();
+      addAstronautForm().should('exist');
+      cy.checkA11y({
+        include: [['app-add-astronaut']],
       });
     });
   });
